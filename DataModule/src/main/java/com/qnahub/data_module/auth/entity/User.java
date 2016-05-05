7056ca80a5829f1d91dd1b02d633f24bfd4872ba
marikth <marikth@gmail.com>
2016-05-06 01:05:10 +0300

    Adding DB creation for CI

--- DataModule/src/main/java/com/qnahub/data_module/auth/entity/User.java
+++ DataModule/src/main/java/com/qnahub/data_module/auth/entity/User.java
@@ -2,19 +2,18 @@ package com.qnahub.data_module.auth.entity;
 
 import com.qnahub.common.entity.SuperEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by markth on 4/13/2016.
  */
 @Entity
+@Table(name = "USER")
 public class User extends SuperEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
 
     public Long getId() {
